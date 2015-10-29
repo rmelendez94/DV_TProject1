3dd6a46b159ae8461fb86512431583081bc78915
Rolando Melendez <rolando.melendez@utexas.edu>
2015-10-29 18:51:16 -0500

    Finished scatter and bar, updated red and remove unnecessary two files leaving only the one. pending cross tab and update of red to finish everything.

---
+++ 03 R SQL Visualizations/ScatterPlot.R
@@ -0,0 +1,41 @@
+require(tidyr)
+require(dplyr)
+require(extrafont)
+require(ggplot2)
+
+dfs <- df %>% select(DURATION, Y, CONS_PRICE_IDX)
+dfsY <- df %>% select(DURATION, Y, CONS_PRICE_IDX) %>% filter(Y='yes')
+dfsN <- df %>% select(DURATION, Y, CONS_PRICE_IDX) %>% filter(Y='no')
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_grid(Y~.) +
+  labs(title='Portuguese Bank Marketing Campaign Effectiveness') +
+  labs(x="Duration", y=paste("Consumer Price Index")) +
+  layer(data=dfs, 
+        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.numeric(as.character(CONS_PRICE_IDX)), color=Y),
+        stat="identity",
+        stat_params=list(),
+        geom="point",
+        geom_params=list(alpha=.8), 
+        position=position_jitter(width=0, height=0)
+  )+
+  layer(data=dfsY, 
+        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.numeric(as.character(CONS_PRICE_IDX)), color=blue),
+        stat="identity",
+        stat_params=list(),
+        geom="smooth",
+        geom_params=list(alpha=.8), 
+        position=position_jitter(width=0, height=0)
+  )+
+  layer(data=dfsN, 
+        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.numeric(as.character(CONS_PRICE_IDX)), color=red),
+        stat="identity",
+        stat_params=list(),
+        geom="smooth",
+        geom_params=list(alpha=.8), 
+        position=position_jitter(width=0, height=0)
+)
+
