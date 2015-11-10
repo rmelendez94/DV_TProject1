160bfebb76873fa6142de42997211bb95e692549
Rolando Melendez <rolando.melendez@utexas.edu>
2015-11-10 12:42:30 -0600

    Finished Scatter Plot Finished Bar Chart Pending fixing the text cutoff issue Updated rmd Knitted new html

--- 03 R SQL Visualizations/Barchart_RLine.R
+++ 03 R SQL Visualizations/Barchart_RLine.R
@@ -5,9 +5,9 @@ require(ggplot2)
 
 dfb <- df %>% group_by(POUTCOME, Y) %>% summarise(AVG_CAMPAIGN = mean(CAMPAIGN))
 dfb1 <- dfb %>% ungroup %>% group_by(POUTCOME) %>% summarise(WINDOW_AVG_CAMPAIGN = mean(AVG_CAMPAIGN))
-dfb <- inner_join(dfb, dfb1, by="POUTCOME")
+dfb <- inner_join(dfb, dfb1, by="POUTCOME") 
 
-spread(dfb, Y, AVG_CAMPAIGN) %>% View
+#spread(dfb, Y, AVG_CAMPAIGN) %>% View
 
 ggplot() + 
   coord_cartesian() + 
@@ -17,35 +17,35 @@ ggplot() +
   labs(title='Portuguese Bank Marketing Campaign Effectiveness\nScatter Plot\nAVG_CAMPAIGN, WINDOW_AVG_CAMPAIGN') +
   labs(x=paste("Y (OUTCOME)"), y=paste("AVG_CAMPAIGN")) +
   layer(data=dfb, 
-        mapping=aes(x=Y, y=AVG_CAMPAIGN), 
+        mapping=aes(x=Y, y=AVG_CAMPAIGN, color=Y, fill=Y), 
         stat="identity", 
         stat_params=list(), 
         geom="bar",
-        geom_params=list(colour="blue", fill="blue"), 
+        geom_params=list(width=.25), 
         position=position_identity()
   ) + coord_flip() +
   layer(data=dfb, 
-        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(AVG_CAMPAIGN)), 
+        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(AVG_CAMPAIGN, 4)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
-        geom_params=list(colour="black", hjust=-0.5), 
+        geom_params=list(colour="black", hjust=-0.5, size=3.5), 
         position=position_identity()
   ) +
   layer(data=dfb, 
-        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(WINDOW_AVG_CAMPAIGN)), 
+        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(WINDOW_AVG_CAMPAIGN, 4)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
-        geom_params=list(colour="black", hjust=-2), 
+        geom_params=list(colour="black", hjust=-2, size=3.5), 
         position=position_identity()
   ) +
   layer(data=dfb, 
-        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(AVG_CAMPAIGN - WINDOW_AVG_CAMPAIGN)), 
+        mapping=aes(x=Y, y=AVG_CAMPAIGN, label=round(AVG_CAMPAIGN - WINDOW_AVG_CAMPAIGN, 4)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
-        geom_params=list(colour="black", hjust=-5), 
+        geom_params=list(colour="black", hjust=-5, size=3.5), 
         position=position_identity()
   ) +
   layer(data=dfb, 
